c730abd871fc83ccdae1843b6f62f9022b0aaaaf
akachiryo <85982768+akachiryo@users.noreply.github.com>
2023-06-21 23:54:48 +0900

    Add:User登録機能　実装完了

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"go-rest-api/controller"
+	"go-rest-api/db"
+	"go-rest-api/repository"
+	"go-rest-api/router"
+	"go-rest-api/usecase"
+	"go-rest-api/validator"
+)
+
+func main() {
+	db := db.NewDB()
+	userValidator := validator.NewUserValidator()
+	userRepository := repository.NewUserRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
+	userController := controller.NewUserController(userUsecase)
+	e := router.NewRouter(userController)
+	e.Logger.Fatal(e.Start(":8080"))
+}
